3d097e8e9944e70bc15f7e50647b4f14e1bedbee
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-12-22 19:49:27 +0200

    Update function.js

--- 01.JS Front-End JS Syntax Fundamentals/function.js
+++ 01.JS Front-End JS Syntax Fundamentals/function.js
@@ -1,5 +1,15 @@
 function calculate(a, b) {
   return a + b;
 }
-
 console.log(calculate(1, 7));
+
+////////////////////////////////
+
+function aaa(text) {
+  return text + " World!";
+}
+
+let result = aaa("Hello,");
+
+console.log(result);
+//console.log(aaa("Hello,"));
