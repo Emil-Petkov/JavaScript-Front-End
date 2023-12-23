750dc97344cca51ec49b2abf0714323b03188740
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-12-23 17:11:32 +0200

    Create 06.largestNumber.js

---
+++ 01.JS Front-End JS Syntax Fundamentals/01.lab/06.largestNumber.js
@@ -0,0 +1,7 @@
+function maxNumber(firstNum, secondNum, thirdNum) {
+  let result = Math.max(firstNum, secondNum, thirdNum);
+
+  console.log(`The largest number is ${result}.`);
+}
+
+maxNumber(5, -3, 16);
