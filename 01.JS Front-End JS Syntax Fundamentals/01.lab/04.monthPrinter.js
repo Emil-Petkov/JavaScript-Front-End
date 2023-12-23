56467ca7339da5f5162a5bf8d9c406e3bb2be371
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-12-23 15:57:46 +0200

    Update 04.monthPrinter.js

--- 01.JS Front-End JS Syntax Fundamentals/01.lab/04.monthPrinter.js
+++ 01.JS Front-End JS Syntax Fundamentals/01.lab/04.monthPrinter.js
@@ -34,55 +34,55 @@
 function monthOfYear(currentMonth) {
   switch (currentMonth) {
     case 1:
-      console.log("January");
+      console.log('January');
       break;
 
     case 2:
-      console.log("February");
+      console.log('February');
       break;
 
     case 3:
-      console.log("March");
+      console.log('March');
       break;
 
     case 4:
-      console.log("April");
+      console.log('April');
       break;
 
     case 5:
-      console.log("May");
+      console.log('May');
       break;
 
     case 6:
-      console.log("June");
+      console.log('June');
       break;
 
     case 7:
-      console.log("July");
+      console.log('July');
       break;
 
     case 8:
-      console.log("August");
+      console.log('August');
       break;
 
     case 9:
-      console.log("September");
+      console.log('September');
       break;
 
     case 10:
-      console.log("October");
+      console.log('October');
       break;
 
     case 11:
-      console.log("November");
+      console.log('November');
       break;
 
     case 12:
-      console.log("December");
+      console.log('December');
       break;
 
     default:
-      console.log("Error!");
+      console.log('Error!');
   }
 }
 
