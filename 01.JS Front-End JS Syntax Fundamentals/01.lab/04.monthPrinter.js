d932fd67b1fb6b3b606b8390253a980491600c08
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-12-23 02:05:58 +0200

    Create 04.monthPrinter.js

---
+++ 01.JS Front-End JS Syntax Fundamentals/01.lab/04.monthPrinter.js
@@ -0,0 +1,31 @@
+function monthOfYear(currentMonth) {
+  if (currentMonth == 1) {
+    console.log("January");
+  } else if (currentMonth == 2) {
+    console.log("February");
+  } else if (currentMonth == 3) {
+    console.log("March");
+  } else if (currentMonth == 4) {
+    console.log("April");
+  } else if (currentMonth == 5) {
+    console.log("May");
+  } else if (currentMonth == 6) {
+    console.log("June");
+  } else if (currentMonth == 7) {
+    console.log("July");
+  } else if (currentMonth == 8) {
+    console.log("August");
+  } else if (currentMonth == 9) {
+    console.log("September");
+  } else if (currentMonth == 10) {
+    console.log("October");
+  } else if (currentMonth == 11) {
+    console.log("November");
+  } else if (currentMonth == 12) {
+    console.log("December");
+  } else {
+    console.log("Error!");
+  }
+}
+
+monthOfYear(3);
