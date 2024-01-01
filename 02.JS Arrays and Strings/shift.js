61e54dcdc7629ba9bf58a54586cedd01bf4368c5
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-02 00:33:27 +0200

    Create shift.js

---
+++ 02.JS Arrays and Strings/shift.js
@@ -0,0 +1,6 @@
+let numbers = [11, 22, 33, 44, 55, 66, 77];
+
+console.log(numbers.length); //7
+console.log(numbers.shift()); //11 -> remove the firs element of the array and save it.
+console.log(numbers.length); //6
+console.log(numbers); //[22, 33, 44, 55, 66, 77]
