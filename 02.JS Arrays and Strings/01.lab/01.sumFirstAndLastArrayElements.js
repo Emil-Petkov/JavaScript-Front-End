91644099fafdcd5de3eb9a7219faf71e60b8bc2c
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-02 00:32:34 +0200

    Create 01.sumFirstAndLastArrayElements.js

---
+++ 02.JS Arrays and Strings/01.lab/01.sumFirstAndLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastArrayElements(arrayNum) {
+  let first = arrayNum[0];
+  let last = arrayNum[arrayNum.length - 1];
+
+  console.log(first + last);
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
