2f4c6335ea34be4ebaaa091c60d01cf15325b02a
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 13:04:54 +0200

    .

--- 02.JS Arrays and Strings/02.reverseAnArrayOfNumbers.js
+++
@@ -1,7 +0,0 @@
-function reverseAnArrayOfNumbers(nElements, arrayOfNummbers) {
-    let firstNumbers = arrayOfNummbers.slice(0, nElements);
-    rev = firstNumbers.reverse()
-    console.log(rev.join(' '))
-}
-
-reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
