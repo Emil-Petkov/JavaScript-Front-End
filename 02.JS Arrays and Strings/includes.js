62989b0a6ac690fe66599e406aa6e89feed4318e
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 13:06:07 +0200

    Create includes.js

---
+++ 02.JS Arrays and Strings/includes.js
@@ -0,0 +1,9 @@
+let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+
+//includes -> takes two arguments searchElement and fromIndex from which to search to the end of the array
+//if fromIndex is empty, starts searching from index 0 by default
+console.log(array.includes('a')); //true -> starts searching from index 0 by default
+console.log(array.includes('a', 0)); //true -> correct index
+console.log(array.includes('e', 4)); //true ->correct index
+console.log(array.includes('e', 1)); //true -> starts searching from index 1 to the end of the array
+console.log(array.includes('e', 5)); //false -> because it has already passed the index
