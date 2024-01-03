401a68a5f9e0fcea20b43b722ad79af952761033
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 14:52:07 +0200

    Create concatenation.js

---
+++ 02.JS Arrays and Strings/manipulatingStrings/concatenation.js
@@ -0,0 +1,12 @@
+let text = 'My name is' + ', ';
+text += 'Emil';
+
+console.log(text); //My name is, Emil
+
+///////////////////////////////////////
+
+let greeting = 'Hello, '; //must we have add space in the string
+let personName = 'Emil';
+
+let result = greeting.concat(personName);
+console.log(result); //Hello, Emil
