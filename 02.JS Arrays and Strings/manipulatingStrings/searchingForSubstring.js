ba62bcbdccf968aaf497f03dd6c674640b44a446
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 14:52:18 +0200

    Create searchingForSubstring.js

---
+++ 02.JS Arrays and Strings/manipulatingStrings/searchingForSubstring.js
@@ -0,0 +1,10 @@
+let text = 'I am JavaScript developer';
+console.log(text.indexOf('Java')); //5 -> Java substring started of index 5
+console.log(text.indexOf('java')); //-1 -> because not find this substring
+
+///////////////////////////////
+
+let textText = 'Intro to programming';
+let lastChar = textText.lastIndexOf('o'); //index 11
+
+console.log(lastChar);
