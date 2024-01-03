50c9adaf7dccb744f699844b4e649e4dbf3d5d7d
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 15:45:49 +0200

    Create 05.censoredWords.js

---
+++ 02.JS Arrays and Strings/01.lab/05.censoredWords.js
@@ -0,0 +1,9 @@
+function censoredWords(text, hiddenWord) {
+  let lengthWord = hiddenWord.length;
+  let replacement = '*'.repeat(lengthWord);
+  let regex = new RegExp(hiddenWord, 'g');
+
+  return text.replace(regex, replacement);
+}
+
+console.log(censoredWords('aaaaaaaa small small aaaaaaa small', 'small')); //aaaaaaaa ***** ***** aaaaaaa *****
