efbea0dd46086d2a06660290de9f2a4d2cf5edc2
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 16:28:56 +0200

    Create trim.js

---
+++ 02.JS Arrays and Strings/manipulatingStrings/trim.js
@@ -0,0 +1,5 @@
+let text = '    Hello there!  ';
+
+console.log(text.trim()); //Hello there!-> removes spaces on left and right
+console.log(text.trimStart()); //Hello there!  - > removes spaces on left
+console.log(text.trimEnd()); //    Hello there! -> removes spaces on right
