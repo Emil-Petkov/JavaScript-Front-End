20f411d8597f2be79c57ab352ea48b2960a78c82
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-03 16:59:43 +0200

    Create 06.countStringOccurrences.js

---
+++ 02.JS Arrays and Strings/01.lab/06.countStringOccurrences.js
@@ -0,0 +1,36 @@
+function countStringOccurrences(sentence, sub) {
+  let words = sentence.split(' ');
+  let counter = 0;
+
+  for (let word of words) {
+    if (word === sub) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+console.log(
+  countStringOccurrences('This is a word and it also is a sentence', 'is')
+);
+
+///////////////////////////////////////////////////////
+
+function countStringOccurrences(sentence, sub) {
+    let counter = 0;
+    let regex = new RegExp(`\\b${sub}\\b`, 'g');
+  
+    let match = regex.exec(sentence);
+    while (match) {
+      counter++;
+      match = regex.exec(sentence);
+    }
+  
+    return counter;
+  }
+  
+  console.log(
+    countStringOccurrences('This is a word and it also is a sentence', 'is')
+  );
+  
