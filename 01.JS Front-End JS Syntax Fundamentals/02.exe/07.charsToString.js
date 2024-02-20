ad91b475bb7ccde8202647d4b5b58a5b4018c965
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-02-20 22:55:18 +0200

    .

--- 01.JS Front-End JS Syntax Fundamentals/02.exe/07.charsToString.js
+++
@@ -1,5 +0,0 @@
-function charsToStrings(fCh, sCh, tCh) {
-  console.log('' + fCh + sCh + tCh);
-}
-
-charsToStrings('%', '2', 'o');
--- 01.JS Front-End JS Syntax Fundamentals/02.exe/09.fruit.js
+++
@@ -1,8 +0,0 @@
-function fruit(typeOfFruit, grams, price) {
-    let convertGramsToKg = (grams / 1000);
-    let neededMoney = convertGramsToKg * price
-
-    console.log(`I need $${neededMoney.toFixed(2)} to buy ${convertGramsToKg.toFixed(2)} kilograms ${typeOfFruit}.`)
-}
-
-fruit('apple', 1563, 2.35);
