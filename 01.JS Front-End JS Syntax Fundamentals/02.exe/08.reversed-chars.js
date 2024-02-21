d233a44b93faf9ff357c08701da54eb6ee21eb73
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-02-21 11:45:41 +0200

    Create 08.reversed-chars.js

---
+++ 01.JS Front-End JS Syntax Fundamentals/02.exe/08.reversed-chars.js
@@ -0,0 +1,7 @@
+function reversedChars(firstChar, secondChar, thirdChar) {
+    let text = firstChar + secondChar + thirdChar;
+
+    console.log(text.split('').reverse().join(' '));
+}
+
+reversedChars('a', 'b', 'c'); // c b a
