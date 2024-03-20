764578f4bf0764f9d7e69c054010292d6e025153
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-03-20 13:13:57 +0200

    Update 07.theatre-promotions.js

--- 01.JS Front-End JS Syntax Fundamentals/01.lab/07.theatre-promotions.js
+++ 01.JS Front-End JS Syntax Fundamentals/01.lab/07.theatre-promotions.js
@@ -1,47 +1,36 @@
-function theatrePromotions(typeOfDay, age) {
-
-    let ticketPrice = 0;
-
-    if (age < 0 || age > 122) {
-        console.log('Error!');
-    } else {
-
+function solve(typeOfDay, age) {
+    if (age >= 0 && age <= 1000) {
         switch (typeOfDay) {
             case 'Weekday':
                 if (age <= 18) {
-                    ticketPrice = 12;
+                    return '12$'
                 } else if (age <= 64) {
-                    ticketPrice = 18;
-                } else {
-                    ticketPrice = 12;
+                    return '18$'
+                } else if (age <= 122) {
+                    return '12$'
                 }
-                break;
 
             case 'Weekend':
                 if (age <= 18) {
-                    ticketPrice = 15;
+                    return '15$'
                 } else if (age <= 64) {
-                    ticketPrice = 20;
-                } else {
-                    ticketPrice = 15;
+                    return '20$'
+                } else if (age <= 122) {
+                    return '15$'
                 }
-                break;
 
             case 'Holiday':
                 if (age <= 18) {
-                    ticketPrice = 5;
+                    return '5$'
                 } else if (age <= 64) {
-                    ticketPrice = 12;
-                } else {
-                    ticketPrice = 10;
+                    return '12$'
+                } else if (age <= 122) {
+                    return '10$'
                 }
-                break;
-
         }
-
-        return console.log(`${ticketPrice}$`);
-
+    } else {
+        return 'Error!'
     }
 }
 
-theatrePromotions('Holiday', -12) //Error!
+console.log(solve('Weekday',));
