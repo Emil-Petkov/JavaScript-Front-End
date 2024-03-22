7d5533df033d0504272d2272f57aa45f98a715dd
emil <E_Petkov@protonmail.com>
2024-03-22 22:16:34 +0200

    Update 02.vacation.js

--- 01.JS Front-End JS Syntax Fundamentals/02.exe/02.vacation.js
+++ 01.JS Front-End JS Syntax Fundamentals/02.exe/02.vacation.js
@@ -1,58 +1,84 @@
-function vacation(nPeople, typeGroup, dayOfWeek) {
-    let totalPrice = 0;
+function solve(peoples, type, day) {
 
-    switch (typeGroup) {
-        case 'Students':
-            if (dayOfWeek === 'Friday') {
-                totalPrice = nPeople * 8.45;
+    let total = 0;
 
-            } else if (dayOfWeek === 'Saturday') {
-                totalPrice = nPeople * 9.80;
-
-            } else if (dayOfWeek === 'Sunday') {
-                totalPrice = nPeople * 10.46;
+    switch (day) {
+        case 'Friday': {
+            if (type == 'Students') {
+                if (peoples >= 30) {
+                    let sum = peoples * 8.45;
+                    total = sum - (sum * 0.15);
+                } else {
+                    total = peoples * 8.45;
+                }
+            } else if (type == 'Business') {
+                if (peoples > 100) {
+                    total = (peoples - 10) * 10.90;
+                } else {
+                    total = peoples * 10.90;
+                }
+            } else if (type == 'Regular') {
+                if (peoples >= 10 && peoples <= 20) {
+                    sum = peoples * 15.00;
+                    total = sum - (sum * 0.05);
+                } else {
+                    total = peoples * 15.00;
+                }
             }
+        }
+            break
 
-            if (nPeople >= 30) {
-                totalPrice -= totalPrice * 0.15;
+        case 'Saturday': {
+            if (type == 'Students') {
+                if (peoples >= 30) {
+                    let sum = peoples * 9.80;
+                    total = sum - (sum * 0.15);
+                } else {
+                    total = peoples * 9.80;
+                }
+            } else if (type == 'Business') {
+                if (peoples > 100) {
+                    total = (peoples - 10) * 15.60;
+                } else {
+                    total = peoples * 15.60;
+                }
+            } else if (type == 'Regular') {
+                if (peoples >= 10 && peoples <= 20) {
+                    sum = peoples * 20.00;
+                    total = sum - (sum * 0.05);
+                } else {
+                    total = peoples * 20.00;
+                }
             }
+        }
             break;
 
-        case 'Business':
-
-            if (nPeople >= 100) {
-                nPeople -= 10
+        case 'Sunday': {
+            if (type == 'Students') {
+                if (peoples >= 30) {
+                    let sum = peoples * 10.46;
+                    total = sum - (sum * 0.15);
+                } else {
+                    total = peoples * 10.46;
+                }
+            } else if (type == 'Business') {
+                if (peoples > 100) {
+                    total = (peoples - 10) * 16.00;
+                } else {
+                    total = peoples * 16.00;
+                }
+            } else if (type == 'Regular') {
+                if (peoples >= 10 && peoples <= 20) {
+                    sum = peoples * 22.50;
+                    total = sum - (sum * 0.05);
+                } else {
+                    total = peoples * 22.50;
+                }
             }
-
-            if (dayOfWeek === 'Friday') {
-                totalPrice = nPeople * 10.90;
-
-            } else if (dayOfWeek === 'Saturday') {
-                totalPrice = nPeople * 15.60;
-
-            } else if (dayOfWeek === 'Sunday') {
-                totalPrice = nPeople * 16.00;
-            }
-            break;
-
-        case 'Regular':
-            if (dayOfWeek === 'Friday') {
-                totalPrice = nPeople * 15.00;
-
-            } else if (dayOfWeek === 'Saturday') {
-                totalPrice = nPeople * 20.00;
-
-            } else if (dayOfWeek === 'Sunday') {
-                totalPrice = nPeople * 22.50;
-            }
-
-            if (nPeople >= 10 && nPeople <= 20) {
-                totalPrice -= totalPrice * 0.05;
-            }
+        }
             break;
     }
-
-    console.log(`Total price: ${totalPrice.toFixed(2)}`);
+    return `Total price: ${total.toFixed(2)}`;
 }
 
-vacation(30, "Students", "Sunday"); //Total price: 266.73
+console.log(solve(40, "Regular", "Saturday"));
