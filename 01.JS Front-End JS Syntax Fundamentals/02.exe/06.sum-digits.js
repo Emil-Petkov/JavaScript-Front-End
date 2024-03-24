2fd7685017637997f5fb157b123f6a06c672d447
emil <emil.ivanchev.petkov@gmail.com>
2024-03-24 11:58:33 +0200

    Update 06.sum-digits.js

--- 01.JS Front-End JS Syntax Fundamentals/02.exe/06.sum-digits.js
+++ 01.JS Front-End JS Syntax Fundamentals/02.exe/06.sum-digits.js
@@ -1,15 +1,11 @@
 function sumDigits(number) {
-    let sumOfDigits = 0;
+    let sum = 0;
 
-    while (number > 0) {
-        sumOfDigits += number % 10;
-        number = Math.floor(number / 10);
-
+    for (let digit of number.toString()) {
+        sum += parseInt(digit);
     }
 
-    console.log(sumOfDigits);
+    return sum;
 }
 
-sumDigits(245678); //32
-sumDigits(97561); //28
-sumDigits(543); //12
+console.log(sumDigits(543));
