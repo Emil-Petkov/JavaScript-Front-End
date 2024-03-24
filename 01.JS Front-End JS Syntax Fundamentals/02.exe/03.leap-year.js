0a3526b57b1ebc55336a5fd27a54b7731d84c3dd
emil <emil.ivanchev.petkov@gmail.com>
2024-03-24 11:36:25 +0200

    Update 03.leap-year.js

--- 01.JS Front-End JS Syntax Fundamentals/02.exe/03.leap-year.js
+++ 01.JS Front-End JS Syntax Fundamentals/02.exe/03.leap-year.js
@@ -1,11 +1,10 @@
 function leapYear(year) {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        console.log('yes');
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return 'yes'
     } else {
-        console.log('no');
+        return 'no'
+
     }
 }
 
-leapYear(1984); //yes
-leapYear(2003); //no
-leapYear(4); //yes
+console.log(leapYear(1992));
