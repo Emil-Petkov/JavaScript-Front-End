d79e578829c7fff7ed80952252b6834e32f0ca1b
emil <emil.ivanchev.petkov@gmail.com>
2024-04-03 01:06:17 +0300

    Update 04.orders.js

--- 03.functions-and-statements/01.lab/04.orders.js
+++ 03.functions-and-statements/01.lab/04.orders.js
@@ -1,32 +1,17 @@
 function solve(product, quantity) {
-    let totalPrice = 0;
 
     const price = {
         coffee: 1.50,
         water: 1.00,
         coke: 1.40,
         snacks: 2.00,
+    };
+
+    if (price[product]) {
+        let totalPrice = quantity * price[product];
+
+        return totalPrice.toFixed(2);
     }
-
-    switch (product) {
-        case 'coffee':
-            totalPrice += quantity * price.coffee;
-            break;
-
-        case 'water':
-            totalPrice += quantity * price.water;
-            break;
-
-        case 'coke':
-            totalPrice += quantity * price.coke;
-            break;
-
-        case 'snacks':
-            totalPrice += quantity * price.snacks;
-            break;
-    }
-
-    return totalPrice.toFixed(2);
 
 };
 
