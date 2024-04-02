e5f859bce384f36963fe9a81c1e8dea2a1a0047d
emil <emil.ivanchev.petkov@gmail.com>
2024-04-02 21:32:14 +0300

    Create 04.sorting-numbers.js

---
+++ 02.JS Arrays and Strings/02.exe/04.sorting-numbers.js
@@ -0,0 +1,17 @@
+function solve(numbers) {
+    let result = [];
+    
+    numbers.sort((a, b) => a - b);
+
+    while (numbers.length > 0) {
+
+        let minNumber = numbers.shift();
+        let maxNumber = numbers.pop();
+
+        result.push(minNumber, maxNumber);
+    }
+
+    return result;
+}
+
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
