34aa8f4b08e15172aa8d3634280a96bf13c42582
emil <emil.ivanchev.petkov@gmail.com>
2024-04-02 14:26:56 +0300

    .

--- 02.JS Arrays and Strings/01.lab/01.sumFirstAndLastArrayElements.js
+++
@@ -1,8 +0,0 @@
-function sumFirstAndLastArrayElements(arrayNum) {
-  let first = arrayNum[0];
-  let last = arrayNum[arrayNum.length - 1];
-
-  console.log(first + last);
-}
-
-sumFirstAndLastArrayElements([20, 30, 40]);
--- 02.JS Arrays and Strings/01.lab/04.substring.js
+++
@@ -1,5 +0,0 @@
-function substring(text, startIndex, endIndex) {
-  return text.substring(startIndex, startIndex + endIndex);
-}
-
-console.log(substring('SkipWord', 4, 7));
