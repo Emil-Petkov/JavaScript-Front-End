530cf462fdfd7350b0c3f975f9e369ed91194bc5
emil <emil.ivanchev.petkov@gmail.com>
2024-04-03 13:11:47 +0300

    Create 07.n-x-n-matrix.js

---
+++ 03.functions-and-statements/02.exe/07.n-x-n-matrix.js
@@ -0,0 +1,27 @@
+function solve(n) {
+    let result = [];
+
+    let numberAsString = String(n);
+    let line = numberAsString.repeat(n);
+
+    let spacedLine = [...line].join(' ');
+
+    for (let i = 0; i < n; i++) {
+        result.push(spacedLine);
+    }
+
+    return result.join('\n');
+};
+
+console.log(solve(3));
+
+////////////////////////////////////////////////
+
+function solve (n) {
+    for (let i = 0; i < n; i++) {
+        row_nums = Array(n).fill(n)
+        console.log(row_nums.join(' '))
+    }
+};
+
+solve(3);
