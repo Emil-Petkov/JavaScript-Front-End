5a0446f723620cf54b9937d37a886855a1e52faf
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-05 15:20:07 +0300

    Create 01.person-info.js

---
+++ 04.objects-and-classes/01.lab/01.person-info.js
@@ -0,0 +1,11 @@
+function solve(firstName, lastName, age) {
+    let info = {
+        firstName,
+        lastName,
+        age,
+    };
+
+    return info;
+}
+
+console.log(solve('Peter', 'Pan', '20'));
