c682e8ccfbea7564918286cdc321488def8541d7
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-08 12:04:42 +0300

    Create 08.cats.js

---
+++ 04.objects-and-classes/01.lab/08.cats.js
@@ -0,0 +1,28 @@
+function solve(array) {
+    let cats = [];
+
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (let index = 0; index < array.length; index++) {
+        const currentCat = array[index].split(' ');
+        const [name, age] = currentCat;
+        const newCat = new Cat(name, age);
+        newCat.meow();
+        cats.push(newCat);
+
+
+    }
+
+}
+
+solve(['Mellow 2', 'Tom 5']);
