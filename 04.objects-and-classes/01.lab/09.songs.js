9ddbef54236ecf728fcecec3a8e251bac6904173
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-08 12:24:04 +0300

    Create 09.songs.js

---
+++ 04.objects-and-classes/01.lab/09.songs.js
@@ -0,0 +1,38 @@
+function solve(array) {
+    class Songs {
+        constructor(type, name, time) {
+            this.type = type;
+            this.name = name;
+            this.time = time;
+        }
+    }
+
+    let songs = [];
+    let numbersOfSongs = array.shift();
+    let typeSong = array.pop();
+
+    for (let i = 0; i < numbersOfSongs; i++) {
+        let [type, name, time] = array[i].split('_');
+        let song = new Songs(type, name, time);
+        songs.push(song);
+
+    }
+
+    if (typeSong === 'all') {
+        songs.forEach((i) => console.log(i.name));
+
+    } else {
+        let filtred = songs.filter((i) => i.type === typeSong);
+        filtred.forEach((i) => console.log(i.name));
+    }
+
+}
+
+solve(
+    [3,
+        'favourite_DownTown_3:14',
+        'favourite_Kiss_4:16',
+        'favourite_Smooth Criminal_4:01',
+        'favourite'
+    ]
+)
